9cc3d5afe99dbee04f99f67d1dafa33df64e5ac4
OMER FARUK GUZEL <58605364+omerfarukgzl@users.noreply.github.com>
2023-06-11 15:46:54 +0300

    start

---
+++ saubank/src/main/java/com/saubank/saubank/SaubankApplication.java
@@ -0,0 +1,13 @@
+package com.saubank.saubank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaubankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaubankApplication.class, args);
+	}
+
+}
---
+++ saubank/src/test/java/com/saubank/saubank/SaubankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saubank.saubank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaubankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
